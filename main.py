68e8b7d1bbd9b30e7af93a37e1a327ae6f760811
Shahmeer0311 <22alis@students.watfordboys.org>
2024-11-16 14:21:41 +0000

    print statement

---
+++ main.py
@@ -0,0 +1,12 @@
+
+print("Hello World") #print the statement 
+print("result of 3 + 5 =",3+5)
+
+print("ALgortihm for addidnt two numbers")
+print("Step 1.start")
+print("Step 2.declare two variables n1 and n2")
+print("Step 3.Read the values")
+print("step 4.n1 + n2")
+print("Step 5.display Output")
+
+
